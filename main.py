275216dcfa8977ab3fc2963e59c0a2a5e4d62b3f
gamwed <14orbit@gmail.com>
2023-05-23 13:54:53 +0300

    Больше не успел

---
+++ main.py
@@ -0,0 +1,44 @@
+# task 1
+# for number in range(1, 101):
+#     if number % 7 == 0:
+#         print(number)
+# task 2
+# sum = 0
+# count = 0
+# for number in range(1, 100):
+#     if number % 2 != 0:
+#         sum += number
+#         count += 1
+# print("Сума цілих непарних чисел від 1 до 99:", sum)
+# print("Кількість цілих непарних чисел від 1 до 99:", count)
+# task 3
+# num = 0
+# while num <= 200:
+#     print(num, end=" ")
+#     if num % 5 == 0:
+#         print()
+#     num += 1
+# task 4
+# n = int(input("Ведите число: "))
+# result = 1
+# for i in range(1, n + 1):
+#     result *= i
+# print(result)
+# task 5
+# for i in range(1, 11):
+#     result = i * 5
+#     print(i, "x 5 =", result)
+# task 6
+# hight = int(input("Висота "))
+# width = int(input("Ширина "))
+# for i in range(hight):
+#     for j in range(width):
+#         print('*', end='')
+#     print()
+# task 7
+# skiki = [0, 5, 2, 4, 7, 1, 3, 19]
+# cont = 0
+# for num in skiki:
+#     if num % 2 != 0:
+#         cont +=1
+# print("непарных: ", cont)
